8a5e9f37fe0e434d9af0293d0061a5914cbc7cae
Van Kichline <van@kichline.com>
2017-12-13 21:04:30 -0800

    hooked up forecast icon

--- src/components/WeatherStatus.vue
+++ src/components/WeatherStatus.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="weather-status">
     <div class="temperature">{{ temp }}&deg;</div>
-    <div class="forecast cloudy"></div>
+    <div v-bind:class="fcast"></div>
   </div>
 </template>
 
@@ -11,7 +11,8 @@ export default {
   data () {
     return {
       timer: null,
-      temp: '?'
+      temp: '?',
+      fcast: ''
     }
   },
   mounted () {
@@ -43,12 +44,75 @@ export default {
       request.open('GET', url)
       request.send()
     },
-
+    getIconClass: function (weatherCode) {
+    // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
+      weatherCode = parseInt(weatherCode)
+      switch (weatherCode) {
+        case 25: // cold
+        case 32: // sunny
+        case 33: // fair (night)
+        case 34: // fair (day)
+        case 36: // hot
+        case 3200: // not available
+          return 'clear-day'
+        case 0: // tornado
+        case 1: // tropical storm
+        case 2: // hurricane
+        case 6: // mixed rain and sleet
+        case 8: // freezing drizzle
+        case 9: // drizzle
+        case 10: // freezing rain
+        case 11: // showers
+        case 12: // showers
+        case 17: // hail
+        case 35: // mixed rain and hail
+        case 39: // scattered showers
+        case 40: // scattered showers
+          return 'rain'
+        case 3: // severe thunderstorms
+        case 4: // thunderstorms
+        case 37: // isolated thunderstorms
+        case 38: // scattered thunderstorms
+        case 45: // thundershowers
+        case 47: // isolated thundershowers
+          return 'thunderstorms'
+        case 5: // mixed rain and snow
+        case 7: // mixed snow and sleet
+        case 13: // snow flurries
+        case 14: // light snow showers
+        case 16: // snow
+        case 18: // sleet
+        case 41: // heavy snow
+        case 42: // scattered snow showers
+        case 43: // heavy snow
+        case 46: // snow showers
+          return 'snow'
+        case 15: // blowing snow
+        case 19: // dust
+        case 20: // foggy
+        case 21: // haze
+        case 22: // smoky
+          return 'fog'
+        case 24: // windy
+        case 23: // blustery
+          return 'windy'
+        case 26: // cloudy
+        case 27: // mostly cloudy (night)
+        case 28: // mostly cloudy (day)
+        case 31: // clear (night)
+          return 'cloudy'
+        case 29: // partly cloudy (night)
+        case 30: // partly cloudy (day)
+        case 44: // partly cloudy
+          return 'partly-cloudy-day'
+      }
+    },
     refreshForecast: function () {
       let that = this
       this.getForecast(function (data) {
+        console.log('xyzzy')
+        that.fcast = 'forecast ' + that.getIconClass(data.channel.item.condition.code)
         that.temp = data.channel.item.condition.temp
-        console.log(this.temp)
       })
     }
   }
