c6d3638c5932937c15d654eb24b295a63146d0df
Van Kichline <van@kichline.com>
2017-12-25 15:39:15 -0800

    functional fix for moon data

--- src/components/SkyClock.vue
+++ src/components/SkyClock.vue
@@ -225,6 +225,12 @@ export default {
                   break
               }
             }
+            // This became necessary on Christmas day 2017. TBD: Why?
+            for (const dat of response.nextmoondata) {
+              if (dat.phen === 'S') {
+                results[2] = this.getDateFromSunMoonData(now, dat.time)
+              }
+            }
             for (const dat of response.sundata) {
               switch (dat.phen) {
                 case 'BC': results[3] = this.getDateFromSunMoonData(now, dat.time)
